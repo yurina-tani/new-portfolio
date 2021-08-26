cac683ce28884cf75de3fa3e2c7400e93154f92e
yurina-tani <84496206+yurina-tani@users.noreply.github.com>
2021-08-26 15:54:38 +0900

    add

--- script.js
+++ script.js
@@ -30,27 +30,29 @@ $("#g-nav a").click(function () { //ナビゲーションのリンクがクリ�
 
 
 //worksのスライドショー
+
+
 $('.slider').slick({
-    autoplay: true, //自動的に動き出すか。初期値はfalse。
-    infinite: true, //スライドをループさせるかどうか。初期値はtrue。
-    slidesToShow: 5, //スライドを画面に3枚見せる
-    slidesToScroll: 1, //1回のスクロールで3枚の写真を移動して見せる
-    prevArrow: '<div class="slick-prev"></div>', //矢印部分PreviewのHTMLを変更
-    nextArrow: '<div class="slick-next"></div>', //矢印部分NextのHTMLを変更
-    dots: true, //下部ドットナビゲーションの表示
+    autoplay: true,//自動的に動き出すか。初期値はfalse。
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    slidesToShow: 5,//スライドを画面に5枚見せる
+    slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+    dots: true,//下部ドットナビゲーションの表示
     responsive: [
         {
-            breakpoint: 769, //モニターの横幅が769px以下の見せ方
+            breakpoint: 769,//モニターの横幅が769px以下の見せ方
             settings: {
-                slidesToShow: 2, //スライドを画面に2枚見せる
-                slidesToScroll: 2, //1回のスクロールで2枚の写真を移動して見せる
+                slidesToShow: 2,//スライドを画面に2枚見せる
+                slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
             }
         },
         {
-            breakpoint: 450, //モニターの横幅が426px以下の見せ方
+            breakpoint: 426,//モニターの横幅が426px以下の見せ方
             settings: {
-                slidesToShow: 1, //スライドを画面に1枚見せる
-                slidesToScroll: 1, //1回のスクロールで1枚の写真を移動して見せる
+                slidesToShow: 1,//スライドを画面に1枚見せる
+                slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
             }
         }
     ]
